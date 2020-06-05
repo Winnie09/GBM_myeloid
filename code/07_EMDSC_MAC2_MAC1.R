503dc88223ffabfb09eb5083604270f8ba21c2f7
Wenpin Hou <wp.hou3@gmail.com>
2020-06-04 21:53:35 -0400

    add changes

--- code/07_EMDSC_MAC2_MAC1.R
+++ code/07_EMDSC_MAC2_MAC1.R
@@ -8,6 +8,11 @@ dir.create(rdir, showWarnings = F, recursive = T)
 setwd(rdir)
 
 cnt <- readRDS('/home-4/whou10@jhu.edu/data2/whou10/GBM/singleObject/M/count.rds')
+# # ### subset a small test set
+# set.seed(12345)
+# id1 = sample(rownames(cnt),3)
+# cnt <- cnt[id1, ]
+##### 
 meta <- readRDS('/home-4/whou10@jhu.edu/data2/whou10/GBM/singleObject/M/meta.rds')
 cnt <- cnt[, pseudotime]
 cellanno <- data.frame(cell = colnames(cnt), sample = sapply(colnames(cnt), function(i) sub('_.*','',sub('.*-','',i))), stringsAsFactors = FALSE)
@@ -22,12 +27,36 @@ cnt <- as.matrix(cnt)
 rc <- colSums(cnt)
 rc <- rc/median(rc)
 cnt <- t(log2(t(cnt)/rc + 1))
-cnt <- cnt[rowSums(cnt>0.1)>0.01,] ## filter genes
+cnt <- cnt[rowMeans(cnt>0.1)>0.01,] ## filter genes
 
-# ### subset a small test set
-# set.seed(12345)
-# id1 = sample(rownames(cnt),5)
-# cnt <- cnt[id1, ]
 ### algo
-final <- DiffPermute(GeneByCellExpr = cnt, pseudotime = pseudotime, design=design, cellanno = cellanno, maxCell=1000, parallel = TRUE, maxCore = 8)
-saveRDS(final, 'final.rds')  
+psn <- seq(1, length(pseudotime))
+names(psn) <- pseudotime
+design = cbind(1, design)
+res <- testpt(expr=cnt,cellanno=cellanno,pseudotime=psn,design=design,ncores=1, permuiter=100) 
+saveRDS(res, 'final.rds') 
+
+# ####### debug input
+# 
+# expr=cnt
+# cellanno=cellanno
+# pseudotime=psn
+# design=design
+# ncores=(detectCores()-2)
+# permuiter=100
+# EMitercutoff=1
+# verbose=F
+# EMmaxiter=100
+# 
+# expr=expr
+# cellanno=cellanno
+# pseudotime=pseudotime
+# design=design
+# EMmaxiter=EMmaxiter
+# EMitercutoff=EMitercutoff
+# verbose=verbose
+# ncores=ncores
+# knotnum=NULL
+# 
+# 
+
