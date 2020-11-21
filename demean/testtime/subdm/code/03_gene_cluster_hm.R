d672d48e3d22ffe5131eea8942ec887c04f0eb9d
Wenpin Hou <wp.hou3@gmail.com>
2020-11-21 16:10:15 -0500

    add changes

--- demean/testtime/subdm/code/03_gene_cluster_hm.R
+++ demean/testtime/subdm/code/03_gene_cluster_hm.R
@@ -11,74 +11,39 @@ library(splines)
 library(viridis)
 source('/home-4/whou10@jhu.edu/scratch/Wenpin/trajectory_variability/function/01_function.R')
 
-#trajectory <- as.character(commandArgs(trailingOnly = TRUE)[[1]][1])
-for (trajectory in c('EMDSC_MAC1','EMDSC_MAC1_MAC2','EMDSC_MMDSC','EMDSC_MMDSC_PMNMDSC','EMDSC_MMDSC_MAC1')) {
+for (trajectory in c('EMDSC_MAC1', 'EMDSC_MAC1_MAC2', 'EMDSC_MMDSC', 'EMDSC_MMDSC_PMNMDSC', 'EMDSC_MMDSC_MAC1')){
   print(trajectory)
   rdir <- paste0('/home-4/whou10@jhu.edu/scratch/Wenpin/GBM_myeloid/demean/testtime/subdm/result/', trajectory, '/res/')
-  datadir <- paste0('/home-4/whou10@jhu.edu/scratch/Wenpin/GBM_myeloid/demean/testtime/subdm/result/', trajectory, '/data/')
   plotdir <- paste0('/home-4/whou10@jhu.edu/scratch/Wenpin/GBM_myeloid/demean/testtime/subdm/plot/', trajectory, '/')
   
   Res <- readRDS(paste0(rdir, 'ptest_res.rds'))
-  res = data.frame(fdr = Res$fdr, foldchange = Res$foldchange, pvalue = Res$pvalue)
-  res = res[res[,1]<0.05, ]
-  res = res[order(res[,1], res[,2]), ]
-  str(res)
-  write.csv(res, paste0(rdir, 'differential_genes.csv'))
-  
   pt = Res$pseudotime
+  
+  
+  res <- read.csv(paste0(rdir, 'differential_genes_clu.csv'), row.names = 1)
+  clu = res$clu
+  names(clu) = rownames(res)
+  
   pred <- Res$predict.values[rownames(res), names(pt)]
-  str(pred)
-  cellanno = Res$cellanno
+  agg <- t(sapply(unique(clu), function(i) colMeans(pred[clu == i, ,drop=F])))
+  rownames(agg) <- paste0('cluster', unique(clu), '(',table(clu), ')')
   
-  active.ident <- readRDS('/home-4/whou10@jhu.edu/work-zfs/whou10/data2/GBM/singleObject/M_active.ident.rds') ###
+  library(ggplot2)
+  pd <- reshape2::melt(agg)
+  pd$pseudotime = pt[pd[,2]]
+  colnames(pd) <- c('cluster', 'cell', 'expression', 'pseudotime')
   
-  
-  # cluster genes
-  set.seed(12345)
-  clu <- kmeans(pred,5)$cluster
-  clu <- sort(clu)
-  table(clu)
-  res$clu = clu[rownames(res)]
-  write.csv(res, paste0(rdir, 'differential_genes_clu.csv'))
-  
-  # annotate rows and columns
-  colann <- data.frame(sample = cellanno[match(cellanno[,1], colnames(pred)),2],
-                       celltype = active.ident[colnames(pred)],stringsAsFactors = F)
-  rownames(colann) = colnames(pred)
-  rowann = data.frame(cluster = as.character(clu),stringsAsFactors = F)
-  rownames(rowann) = names(clu)
-  
-  # define colors
-  library(pheatmap)
-  col.clu = brewer.pal(length(unique(clu)), 'Set1')
-  names(col.clu) = unique(clu)
-  col.ct = brewer.pal(length(unique(colann$celltype)), name = 'Dark2')
-  names(col.ct) = unique(colann$celltype)
-  col.ct = col.ct[!is.na(names(col.ct))]
-  
-  col.sample = colorRampPalette(rev(brewer.pal(n = 7, name = "Set1")))(length(unique(colann$sample)))
-  names(col.sample) = unique(colann$sample)
-  
-  if (trajectory == 'EMDSC_MMDSC_PMNMDSC'){
-    png(paste0(plotdir, 'hm_kmeans.png'),width=1575,height=2400, res=250)
-  } else {
-    png(paste0(plotdir, 'hm_kmeans.png'),width=1050,height=1600, res=200)
-  }
-  cpl = colorRampPalette(rev(brewer.pal(n = 7, name = "RdYlBu")))(100)
-  # cpl = c(rep(cpl[1], 100), cpl, rep(cpl[length(cpl)], 100)) ## EMDSC_MAC1
-  # cpl = c(rep(cpl[1], 100), cpl, rep(cpl[length(cpl)], 100)) ## EMDSC_MAC1_MAC2
-  # cpl = c(rep(cpl[1], 100), cpl, rep(cpl[length(cpl)], 60)) ## EMDSC_MMDSC
-  # cpl = c(rep(cpl[1], 100), cpl, rep(cpl[length(cpl)], 100)) ## EMDSC_MMDSC_PMNMDSC
-  cpl = c(rep(cpl[1], 100), cpl, rep(cpl[length(cpl)], 100)) ## EMDSC_MMDSC-MAC1
-  
-  pheatmap(pred[names(clu), ], 
-           cluster_rows = F, cluster_cols = FALSE,
-           show_rownames = FALSE, show_colnames = FALSE,
-           color = cpl,
-           annotation_row = rowann, annotation_col = colann,
-           annotation_colors = list(cluster = col.clu, celltype = col.ct, sample = col.sample),
-           cellwidth = 241.24/ncol(pred), cellheight = 467.35/nrow(pred))
+  cluord <- order(as.numeric(gsub('\\(.*', '',sub('cluster', '', unique(pd$cluster)))))
+  pd$cluster <- factor(as.character(pd$cluster), levels =  unique(pd$cluster)[cluord])
+    
+  pdf(paste0(plotdir, 'gene_cluster_pattern.pdf'), width = 4.5, height = 3.2)  
+  print(ggplot(data= pd, aes(x = pseudotime, y = expression, group = cluster, color = cluster)) + 
+    geom_smooth() +
+    theme_classic() +
+    scale_color_brewer(palette = 'Set1') +
+    xlab('Pseudotime') +
+    ylab('Averaged Fitted Centered Expression'))
   dev.off()
 }
-
+  
 
